b8dfb99918299fb785d88d38a5674275f332cdb2
Shifu Wu <shifu.wu@thoughtworks.com>
2019-03-02 14:53:53 +0800

    init commit

---
+++ app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'index page'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
